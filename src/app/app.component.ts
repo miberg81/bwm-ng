a63f233e2d91a35eb2b1c871fa6a75dcd10b7b72
miberg81 <bergermichael81@gmail.com>
2018-11-09 14:15:21 +0200

    Adds header component and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bwm-ng';
+  componentTitle="I'm a component from component.ts";
+  clickHandler(){
+    alert("I'm clicked!");
+  }
 }
+
+
